4e3c2aefc6a432a9771c8ae1b45818661a6bd211
Graeme Gossel <graeme.gossel@gmail.com>
2021-09-05 16:09:19 +1000

    fix pageid routing

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
+import requests
 import wikipedia
-year = input("WHAT YEAR IS IT?! ")
-print(year)
+import json
+# year = input("WHAT YEAR IS IT?! ")
+# print(year)
+year = 1776
 
-page = wikipedia.page(str(year))
+# req = str(requests.get(f"https://en.wikipedia.org/wiki/{year}").content)
+pageid_json = json.loads(requests.get(f"https://en.wikipedia.org/w/api.php?action=query&titles={year}&format=json").content)
+pageid = list(pageid_json['query']['pages'].keys())[0]
+page = wikipedia.page(pageid=pageid)
+assert page.url == f"https://en.wikipedia.org/wiki/{year}"
+
 months =  ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December'] 
 content = page.content.split("\n")
 # print(content)
